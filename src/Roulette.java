d8fefc436ee98412e13cff18e4b60573d8666902
Сергей <mymail@neskazhu.com>
2023-04-29 20:04:20 +0300

    done fine

--- src/Roulette.java
+++ src/Roulette.java
@@ -7,48 +7,47 @@ public class Roulette {
         int randomInt = random.nextInt((max - min) + 1) + min;
         return randomInt;
     }
-
     public static boolean logic(int b, int c, int d) {
-        boolean stavkaIsTrue = false;
+        boolean betIsTrue = false;
         switch (b) {
-            case 1:
+            case 1 -> {
                 if (c == d) {
-                    stavkaIsTrue = true;
+                    betIsTrue = true;
                 }
-                break;
-            case 2:
+            }
+            case 2 -> {
                 if (d % 2 == 0) {
-                    stavkaIsTrue = true;
+                    betIsTrue = true;
                 }
-                break;
-            case 3:
+            }
+            case 3 -> {
                 if (d % 2 == 1) {
-                    stavkaIsTrue = true;
+                    betIsTrue = true;
                 }
-                break;
-            case 4:
+            }
+            case 4 -> {
                 if (d > 18) {
-                    stavkaIsTrue = true;
+                    betIsTrue = true;
                 }
-                break;
-            case 5:
+            }
+            case 5 -> {
                 if (d < 18) {
-                    stavkaIsTrue = true;
+                    betIsTrue = true;
                 }
-                break;
+            }
         }
-        return stavkaIsTrue;
+        return betIsTrue;
     }
 
     public static boolean inputOutput() {
         Scanner sc = new Scanner(System.in);
         System.out.println("Введите ваш тип ставки: конкретное число (1), четное (2), нечетное (3), больше 18 (4), меньше 18(5)");
-        int stavkaType = sc.nextInt();
+        int betType = sc.nextInt();
         System.out.println("Введите вашу ставку: от 0 до 36");
-        int stavka = sc.nextInt();
+        int bet = sc.nextInt();
         int computerNumber = generate(0, 36);
         System.out.println("Число от компьютера: " + computerNumber);
-        boolean logicTrue = Roulette.logic(stavkaType, stavka, computerNumber);
+        boolean logicTrue = Roulette.logic(betType, bet, computerNumber);
         System.out.println("Ставка " + logicTrue);
         return logicTrue;
     }
@@ -57,7 +56,7 @@ public class Roulette {
         int score = 100;
         while (score > 0 && score < 1500) {
             boolean tempBool = Roulette.inputOutput();
-            if (tempBool == true) {
+            if (tempBool) {
                 score += 1;
             } else score -= 1;
             System.out.println("Ваши очки: " + score);
