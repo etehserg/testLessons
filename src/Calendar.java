3818c52b34b7c725097f8f4ce00169a428a19e7c
Сергей <mymail@neskazhu.com>
2023-05-04 18:00:02 +0300

    done fine

--- src/Calendar.java
+++ src/Calendar.java
@@ -25,30 +25,14 @@ public class Calendar {
 
     public static void calendar2 (int daysNumber2){
         switch (daysNumber2) {
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default:
-                System.out.println("Wrong number");
-                break;
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+            default -> System.out.println("Wrong number");
         }
     }
 }
