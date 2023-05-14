e873ddbbd33572f39e52e41c38b0d48e5f59bf3a
Сергей <mymail@neskazhu.com>
2023-05-14 21:00:12 +0300

    done

--- src/Date1.java
+++ src/Date1.java
@@ -1,33 +1,12 @@
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
 
 public class Date1 {
     public static void main(String[] args) {
-        Date date1 = new Date();
-        SimpleDateFormat format1;
-        format1 = new SimpleDateFormat("yyyy лет MM месяцев dd дней hh часов mm минут ss секунд");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Введите дату в мс");
-        long date1User = sc.nextLong();
-        date1.setTime(date1User);
-        System.out.println("Введенная дата: " + format1.format(date1));
-        System.out.println("Количество мс: " + date1.getTime());
-        System.out.println();
-
-        Date date2 = new Date();
-        System.out.println("Введите дату в мс");
-        long date2User = sc.nextLong();
-        date2.setTime(date2User);
-        System.out.println("Введенная дата: " + format1.format(date2));
-        System.out.println("Количество мс: " + date2.getTime());
-        System.out.println();
-
-        Date date3 = new Date();
-        date3.setTime(date2User-date1User);
-        System.out.println("Разница между датами: " + format1.format(date3));
-        System.out.println("Количество мс: " + date3.getTime());
-        System.out.println();
-
+        Calendar today = new GregorianCalendar();
+        Calendar user1 = new GregorianCalendar(2024, 05, 14);
+        today.add(Calendar.DAY_OF_YEAR, 2); //+2 days
+        System.out.println("Сегодня + 2 дня = " + today.getTime());   // today +2
     }
 }
