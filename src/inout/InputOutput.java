5f1a555b847953506a06ee4087aaa6dbaa7c5d34
Сергей <mymail@neskazhu.com>
2023-05-18 21:16:20 +0300

    done with primitive types

---
+++ src/inout/InputOutput.java
@@ -0,0 +1,43 @@
+package inout;
+
+/*[Input/Output] Write into and then read from a file:
+primitive types: boolean, float, integer (one file, same order)
+Strings (text data) - we did a similar exercise in a Games library
+Objects (you can directly write and read complete objects without serializing them into strings) using the ObjectInputStream and ObjectOutputStream
+*/
+
+import java.io.*;
+
+public class InputOutput {
+    public static void main(String[] args) {
+
+       //write static values
+        boolean bool = true;
+        float fl = -10.235f;
+        int i = 10500;
+        File file = new File("primitive.bin");
+        try {
+            FileOutputStream fos = new FileOutputStream(file);
+            DataOutputStream dos = new DataOutputStream(fos);
+            dos.writeBoolean(bool);
+            dos.writeFloat(fl);
+            dos.writeInt(i);
+            dos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //read static values from file
+        File newFile = new File("primitive.bin");
+        try {
+            FileInputStream fos = new FileInputStream(newFile);
+            DataInputStream dos = new DataInputStream(fos);
+            System.out.println(dos.readBoolean());
+            System.out.println(dos.readFloat());
+            System.out.println(dos.readInt());
+            dos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
