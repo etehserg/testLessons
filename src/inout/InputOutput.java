b3b68168412cec8c4e9bf94e5746413288dfdb75
Сергей <mymail@neskazhu.com>
2023-05-20 20:20:44 +0300

    done with objects

--- src/inout/InputOutput.java
+++ src/inout/InputOutput.java
@@ -1,7 +1,6 @@
 package inout;
 
 /*[Input/Output] Write into and then read from a file:
-Strings (text data) - we did a similar exercise in a Games library
 Objects (you can directly write and read complete objects without serializing them into strings) using the ObjectInputStream and ObjectOutputStream
 */
 
